6b9fdf4f809c8b76a7cebfd129d67d01bdbef451
Andres Felipe Ruiz-Hurtado <afruizh1@gmail.com>
2024-12-09 15:41:10 -0500

    fix Layer extent processing

--- src/tree_eyed/tree_eyed.py
+++ src/tree_eyed/tree_eyed.py
@@ -49,6 +49,9 @@ from qgis.core import Qgis
 
 #from qgis.analysis import QgsZonalStatistics
 
+from qgis.core import QgsCoordinateTransform
+from qgis.core import QgsCoordinateReferenceSystem
+
 from .process.gui_utils.config import *
 from .process.gui_utils.qgis_utils import *
 
@@ -751,15 +754,24 @@ class TreeEyed:
             layer = parameters["layer"]
             img = None
 
-            extent = parameters["extent"]
+            extent = layer.extent()
             espg = parameters["extent_crs"].authid()
             
             layer_path = layer.dataProvider().dataSourceUri()
             
             img = cv.imread(layer_path)
-            
 
-            return
+            #Correct extent
+            # Create a QgsCoordinateTransform object
+            crs_source = layer.crs()  # The source CRS of the layer
+            crs_destination = QgsCoordinateReferenceSystem("EPSG:4326")  # The destination CRS
+            transform = QgsCoordinateTransform(crs_source, crs_destination, QgsProject.instance())
+
+            # Transform the extent to EPSG:4326
+            extent_transformed = transform.transformBoundingBox(extent)
+
+            extent = extent_transformed
+
         elif extent_type == "Custom extent":
 
             layer = parameters["layer"]
